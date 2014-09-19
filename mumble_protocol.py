7b63e9f3d174d3998dab639d00199f39e0dfc536
Robert Weidlich <robert@rom.localdomain>
2014-09-19 16:09:10 +0200

    Added parsing for audio frames

--- mumble_protocol.py
+++ mumble_protocol.py
@@ -28,7 +28,7 @@ log = logging.getLogger(__name__)
 class MumbleProtocol(protocol.Protocol):
     VERSION_MAJOR = 1
     VERSION_MINOR = 2
-    VERSION_PATCH = 3
+    VERSION_PATCH = 4
 
     VERSION_DATA = (VERSION_MAJOR << 16) \
                     | (VERSION_MINOR << 8) \
@@ -79,6 +79,7 @@ class MumbleProtocol(protocol.Protocol):
         self.username = username
         self.password = password
         self.tokens = []
+        self.channels = {}
 
     def recvProtobuf(self, msg_type, message):
         log.debug("Received message '%s' (%d):\n%s" \
@@ -99,7 +100,7 @@ class MumbleProtocol(protocol.Protocol):
                                        MumbleProtocol.VERSION_MINOR,
                                        MumbleProtocol.VERSION_PATCH)
         version.os = platform.system()
-        version.os_version = "Mumble 1.2.3 Twisted Protocol"
+        version.os_version = "Mumble 1.2.4 Twisted Protocol"
 
         # Here we authenticate
         auth = Mumble_pb2.Authenticate()
@@ -108,6 +109,7 @@ class MumbleProtocol(protocol.Protocol):
             auth.password = self.password
         for token in self.tokens:
             auth.tokens.append(token)
+        auth.opus = True
 
         # And now we send both packets one after another
         self.sendProtobuf(version)
@@ -118,7 +120,7 @@ class MumbleProtocol(protocol.Protocol):
 
     def init_ping(self):
         # Call ping every PING_REPEAT_TIME seconds.
-        reactor.callLater(MumbleProtocol.PING_REPEAT_TIME, self.ping_handler)
+        self.ping = reactor.callLater(MumbleProtocol.PING_REPEAT_TIME, self.ping_handler)
 
     def ping_handler(self):
         log.debug("Sending ping")
@@ -158,13 +160,23 @@ class MumbleProtocol(protocol.Protocol):
 
             # Read the specific message
             msg = MumbleProtocol.ID_MESSAGE[msg_type]()
-            msg.ParseFromString(
-                self.received[MumbleProtocol.PREFIX_LENGTH:
-                              MumbleProtocol.PREFIX_LENGTH + length])
+            callback_name = "msg_%s" % msg.__class__.__name__
+            # Parse all messages except UDP packages packed in TCP
+            if msg_type != 1:
+                msg.ParseFromString(
+                    self.received[MumbleProtocol.PREFIX_LENGTH:
+                                  MumbleProtocol.PREFIX_LENGTH + length])
+            else:
+                msg = self.received[MumbleProtocol.PREFIX_LENGTH:
+                                    MumbleProtocol.PREFIX_LENGTH + length]
 
             # Handle the message
+            log.debug("Message name: %s" % msg.__class__.__name__)
             try:
-                self.recvProtobuf(msg_type, msg)
+                if hasattr(self, callback_name):
+                    getattr(self, callback_name)(msg_type, msg)
+                else:
+                    self.recvProtobuf(msg_type, msg)
             except Exception:
                 log.error("Exception while handling data.")
                 # We abort on exception, because that's the proper thing to do
@@ -172,6 +184,102 @@ class MumbleProtocol(protocol.Protocol):
                 raise
 
             self.received = self.received[full_length:]
+
+    def msg_Version(self, msg_type, msg):
+        version = "%i.%i.%i" % (msg.version >> 16, (msg.version >> 8) & 0xFF, msg.version & 0xFF)
+        self.got_version(version, msg.release, msg.os, msg.os_version)
+        
+    def got_version(self, version, release, os, os_version):
+        log.debug("Got Version: %s, %s, %s, %s" % (version, release, os, os_version))
+
+    def msg_ChannelState(self, msg_type, msg):
+        channel_name = msg.name
+        channel_id = msg.channel_id
+        channel_parent = msg.parent
+        channel_position = msg.position
+        self.got_one_channel(channel_name, channel_id, channel_parent, channel_position)
+
+    def got_one_channel(self, name, id, parent, position):
+        self.channels[name] = id
+
+    def msg_ServerSync(self, msg_type, msg):
+        self.session = msg.session
+        # TODO: decode permission (no doc on this)
+        self.got_server_sync(msg.session, msg.max_bandwidth, msg.welcome_text)
+        
+    def got_server_sync(self, session, max_bandwidth, welcome_text):
+        log.info("Server says: %s" % welcome_text)
+        print self.channels
+        if "Hilfe und Support ~" in self.channels:
+            self.join_channel(self.session, self.channels['Hilfe und Support ~'])
+
+    def join_channel(self, session, channel_id):
+        state = Mumble_pb2.UserState()
+        state.session = session
+        state.channel_id = channel_id
+        self.sendProtobuf(state)
+
+    def decode_varint(self, m, si=0):
+        v = ord(m[si])
+        if ((v & 0x80) == 0x00):
+            return ((v & 0x7F),1)
+        elif ((v & 0xC0) == 0x80):
+            return ((v & 0x4F) << 8 | ord(m[si+1]),2)
+        elif ((v & 0xF0) == 0xF0):
+            if ((v & 0xFC) == 0xF0):
+                return (ord(m[si+1]) << 24 | ord(m[si+2]) << 16 | ord(m[si+3]) << 8 | ord(m[si+4]), 5)
+            elif ((v & 0xFC) == 0xF4):
+                return (ord(m[si+1]) << 56 | ord(m[si+2]) << 48 | ord(m[si+3]) << 40 | ord(m[si+4]) << 32 | ord(m[si+5]) << 24 | ord(m[si+6]) << 16 | ord(m[si+7]) << 8 | ord(m[si+8]),9)
+            elif ((v & 0xFC) == 0xF8):
+                result, length = self.decode_varint(m, si+1)
+                return(-result, length+1)
+            elif ((v & 0xFC) == 0xFC):
+                return (-(v & 0x03), 1)
+            else:
+                raise Exception("Error while decoding varint 1")
+        elif ((v & 0xF0) == 0xE0):
+            return ((v & 0x0F) << 24 | ord(m[si+1]) << 16 | ord(m[si+2]) << 8 | ord(m[si+3]), 4)
+        elif ((v & 0xE0) == 0xC0):
+            return ((v & 0x1F) << 16 | ord(m[si+1]) << 8 | ord(m[si+2]), 3)
+        else:
+            raise Exception("Error while decoding varint 2")
+
+    def msg_UDPTunnel(self, msg_type, msg):
+        pos = 0
+
+        type_target = ord(msg[pos]) #struct.unpack(">b", msg[pos])[0]
+        type_of_packet = type_target >> 5 # filter out type
+        target = type_target & 0b00011111 # filter out target
+        pos += 1
+        
+        session, n_session_bytes = self.decode_varint(msg[pos:])
+        pos += n_session_bytes
+        sequence, n_sequence_bytes = self.decode_varint(msg[pos:])
+        pos += n_sequence_bytes
+
+        if type_of_packet == 4: # opus has just one packet per message
+            terminator = 1
+            length, n_length = self.decode_varint(msg[pos:])
+            pos += n_length
+        else:
+            terminator_length = ord(msg[pos]) #struct.unpack(">b", msg[pos])[0]
+            terminator = terminator_length >> 7;
+            length = terminator_length & 0b01111111
+            pos += 1
+
+        audio_data = msg[pos:pos+length]
+
+        log.debug("UDP type=%i target=%i session=%i sequence=%i terminator=%i length=%i" % (type_of_packet, target, session, sequence, terminator, length))
+
+        self.handle_audio_data(type_of_packet, target, session, sequence, terminator, audio_data)
+
+    def handle_audio_data(self, type_of_pkg, target, session, sequence, terminator, audio_data):
+        f=open("out/audio-%i-%06i-%03i.celt" % (type_of_pkg, session, sequence), "a")
+        f.write(audio_data)
+        f.close()
+
+    def connectionLost(self, reason):
+        self.ping.cancel()
 
     def sendProtobuf(self, message):
         # We find the message ID
@@ -234,6 +342,7 @@ def main():
 
     mumble_connect(opt.server, opt.port, factory)
     reactor.run()
+
 
 
 if __name__ == '__main__':
